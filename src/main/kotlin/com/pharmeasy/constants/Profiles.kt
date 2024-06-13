800f9b1df197716be91b0d73ebaf4e06bd59652b
RithikKumar07 <rithik.kumar@aknamed.com>
2024-06-13 08:52:38 +0530

    Add dependency to build.gradle.kts and add new OpenAPI configuration file

---
+++ src/main/kotlin/com/pharmeasy/constants/Profiles.kt
@@ -0,0 +1,12 @@
+ 
+package com.pharmeasy.constants
+
+class Profiles {
+        companion object {
+        const val DEV = "dev"
+        const val QA =  "qa"
+        const val QA2 =  "qa2"
+        const val STAGING = "staging"
+        const val PRODUCTION = "production"
+    }
+}
